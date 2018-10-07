a52f723a3e5f6add889a92754980880d96160763
Émy <oniricorpe@disroot.org>
2018-10-07 15:27:07 +0200

    Change for using SQLite instead of Postgresql

--- fosspay/database.py
+++ fosspay/database.py
@@ -3,7 +3,10 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 from .config import _cfg, _cfgi
-engine = create_engine(_cfg('connection-string'))
+from sqlite3 import dbapi2 as sqlite
+
+engine = create_engine(_cfg('connection-string'), module=sqlite)
+
 db = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
